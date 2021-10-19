106eccf558c9faad88e6a56740f64620015213f1
aminnairi <18418459+aminnairi@users.noreply.github.com>
2021-10-19 22:31:52 +0200

    added overrides for rollup and json

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,42 @@ module.exports = {
     "node": true,
     "es2021": true
   },
-  "extends": "./sources/index.js"
+  "overrides": [
+    {
+      "files": "rollup.config.js",
+      "parserOptions": {
+        "sourceType": "module"
+      },
+      "extends": "./sources/index.js"
+    },
+    {
+      "files": "*.js",
+      "extends": "./sources/index.js"
+    },
+    {
+      "files": "*.json",
+      "extends": "plugin:json/recommended",
+      "rules": {
+        "json/undefined": "error",
+        "json/enum-value-mismatch": "error",
+        "json/unexpected-end-of-comment": "error",
+        "json/unexpected-end-of-string": "error",
+        "json/unexpected-end-of-number": "error",
+        "json/invalid-unicode": "error",
+        "json/invalid-escape-character": "error",
+        "json/invalid-character": "error",
+        "json/property-expected": "error",
+        "json/comma-expected": "error",
+        "json/colon-expected": "error",
+        "json/value-expected": "error",
+        "json/comma-or-close-backet-expected": "error",
+        "json/comma-or-close-brace-expected": "error",
+        "json/trailing-comma": "error",
+        "json/duplicate-key": "error",
+        "json/comment-not-permitted": "error",
+        "json/schema-resolve-error": "error",
+        "json/unknown": "error"
+      }
+    }
+  ]
 };
