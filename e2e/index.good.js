1ae735d5b4081206cf6fc505f093a82aacf5c5a5
aminnairi <18418459+aminnairi@users.noreply.github.com>
2021-10-31 11:08:32 +0100

    initial commit

---
+++ e2e/index.good.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const fibonacci = (value) => {
+  if (value <= 0) {
+    return 0;
+  }
+
+  if (value === 1) {
+    return value;
+  }
+
+  return fibonacci(value - 1) + fibonacci(value - 2);
+};
+
+fibonacci(12);
